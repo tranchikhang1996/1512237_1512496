d357b33ec3106a798cba9070b775d5772ab74cef
khangtc2 <khangtc2@vng.com.vn>
2019-05-12 12:20:58 +0700

    update nested scroll view and indicator

--- app/src/main/java/com/example/highschoolmathsolver/customui/OverlayView.kt
+++ app/src/main/java/com/example/highschoolmathsolver/customui/OverlayView.kt
@@ -9,32 +9,43 @@ import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import com.example.highschoolmathsolver.R
-
+import timber.log.Timber
+import kotlin.math.abs
 
 class OverlayView : ImageView {
 
     private var rect: RectF? = null
     private var rectWidth = 200f
     private var rectHeight = 100f
-    private var color = Color.BLACK
-    private val paint = Paint(ANTI_ALIAS_FLAG)
-    private val fillPaint = Paint()
-    private val linePaint = Paint()
-    private val mode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+    private var frameColor = Color.WHITE
+    private var overlayColor = Color.BLACK
+    private var indicatorColor = Color.WHITE
+    private var indicatorThickness = 1f
+    private var frame = 1
+    private val eraser = Paint(ANTI_ALIAS_FLAG).apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
+    private val drawPaint = Paint()
+    private val framePaint = Paint()
+    private val indicatorPaint : Paint by lazy { Paint().apply {
+        color = indicatorColor
+        strokeWidth = indicatorThickness
+    } }
     private var scale_X = 1f
     private var scale_Y = 1f
-    private lateinit var sgd : ScaleGestureDetector
+    private lateinit var sgd: ScaleGestureDetector
+    private var indicatorRunning : Boolean = true
+    private var current = 0f
+    private var shouldStopIndicate : Boolean = true
 
-    val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+    private val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
-            if(Math.abs(detector.currentSpanX - detector.previousSpanX) > Math.abs(detector.currentSpanY - detector.previousSpanY)) {
+            if (Math.abs(detector.currentSpanX - detector.previousSpanX) > Math.abs(detector.currentSpanY - detector.previousSpanY)) {
                 scale_X *= detector.scaleFactor
-                scale_X = Math.max(0.5f, Math.min(scale_X, 2f))
-            }
-            else {
+                scale_X = Math.max(0.5f, Math.min(scale_X, 1.2f))
+            } else {
                 scale_Y *= detector.scaleFactor
                 scale_Y = Math.max(0.5f, Math.min(scale_Y, 2f))
             }
+            indicatorRunning = false
             invalidate()
             return true
         }
@@ -48,30 +59,31 @@ class OverlayView : ImageView {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.OverlayView)
         rectWidth = typedArray.getDimensionPixelSize(R.styleable.OverlayView_window_width, 0).toFloat()
         rectHeight = typedArray.getDimensionPixelSize(R.styleable.OverlayView_window_height, 0).toFloat()
-        color = typedArray.getColor(R.styleable.OverlayView_color, Color.BLACK)
+        indicatorColor = typedArray.getColor(R.styleable.OverlayView_line_color, Color.WHITE)
+        indicatorThickness = typedArray.getDimensionPixelSize(R.styleable.OverlayView_line_thickness, 1).toFloat()
+        frame = typedArray.getInt(R.styleable.OverlayView_line_speed, 1)
+        overlayColor = typedArray.getColor(R.styleable.OverlayView_color, Color.BLACK)
+        frameColor = typedArray.getColor(R.styleable.OverlayView_frame_color, Color.WHITE)
         typedArray.recycle()
         initView()
-    }
-
-    fun getRectWidth() : Float = rectWidth * scale_X
-
-    fun getRectHeight() : Float = rectHeight * scale_Y
-
-    fun setRect(rect: RectF, width: Int, height: Int) {
-        this.rect = rect
-        this.rectWidth = width.toFloat()
-        this.rectHeight = height.toFloat()
-
-        postInvalidate()
     }
 
     private fun initView() {
         sgd = ScaleGestureDetector(context, listener)
         setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-        fillPaint.color = color
+        drawPaint.color = overlayColor
         rect = RectF(0f, 0f, 0f, 0f)
-        linePaint.color = Color.WHITE
-        linePaint.strokeWidth = 4f
+        framePaint.color = frameColor
+        framePaint.strokeWidth = 8f
+    }
+
+    fun stopIndicate() {
+        shouldStopIndicate = true
+    }
+
+    fun startIndicator() {
+        shouldStopIndicate = false
+        invalidate()
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -81,20 +93,54 @@ class OverlayView : ImageView {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        fillPaint.style = Paint.Style.FILL
-        canvas.drawPaint(fillPaint)
+        drawPaint.style = Paint.Style.FILL
+        canvas.drawPaint(drawPaint)
 
         rect?.let {
-            it.left = (width - rectWidth *scale_X) / 2
-            it.right = it.left + rectWidth*scale_X
-            it.top = (height - rectHeight*scale_Y) / 2
-            it.bottom = it.top + rectHeight*scale_Y
-            paint.xfermode = mode
-            canvas.drawRect(it, paint)
-            canvas.drawLine(it.left, it.top, it.right, it.top, linePaint)
-            canvas.drawLine(it.right, it.top, it.right, it.bottom, linePaint)
-            canvas.drawLine(it.right, it.bottom, it.left, it.bottom, linePaint)
-            canvas.drawLine(it.left, it.bottom, it.left, it.top, linePaint)
+            it.left = (width - rectWidth * scale_X) / 2
+            it.right = it.left + rectWidth * scale_X
+            it.top = (height - rectHeight * scale_Y) / 2
+            it.bottom = it.top + rectHeight * scale_Y
+            val radiusX = it.width() / 10
+            val radiusY = it.height() / 10
+            canvas.drawRect(it, eraser)
+            canvas.let { paper ->
+                drawCorner(paper, it.left, it.top, 1, 1, radiusX, radiusY)
+                drawCorner(paper, it.right, it.top, -1, 1, radiusX, radiusY)
+                drawCorner(paper, it.right, it.bottom, -1, -1, radiusX, radiusY)
+                drawCorner(paper, it.left, it.bottom, 1, -1, radiusX, radiusY)
+                if(indicatorRunning) {
+                    drawIndicator(paper, it)
+                } else {
+                    current = 0f
+                    frame = abs(frame)
+                    indicatorRunning = true
+                }
+            }
+            if(!shouldStopIndicate) {
+                invalidate()
+            }
         }
     }
+
+    private fun drawCorner(canvas: Canvas, x: Float, y: Float, dx: Int, dy: Int, radiusX: Float, radiusY: Float) {
+        canvas.drawLine(x, y + dy * 4, x + dx * radiusX, y + dy *4, framePaint)
+        canvas.drawLine(x + dx * 4, y, x + dx * 4, y + dy * radiusY, framePaint)
+    }
+
+    private fun drawIndicator(canvas: Canvas, rect : RectF) {
+        val y = when {
+            rect.top + current + frame > rect.bottom -> rect.bottom
+            rect.top + current + frame < rect.top -> rect.top
+            else -> rect.top + current + frame
+        }
+        if (y == rect.top || y == rect.bottom) {
+            frame = -frame
+        }
+        current = y - rect.top
+        Timber.d("ve hinh chu nhat %f %d , %f , %f, %f %f",current, frame, rect.left, y, rect.right, y)
+        canvas.drawLine(rect.left, y, rect.right, y, indicatorPaint)
+    }
+
+
 }
