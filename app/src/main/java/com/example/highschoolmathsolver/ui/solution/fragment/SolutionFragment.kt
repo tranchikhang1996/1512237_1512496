8099005cb0b620ab33fc7a6afded15969a4a44b8
khangtc2 <khangtc2@vng.com.vn>
2019-05-29 23:10:53 +0700

    update show with m for solution screen

--- app/src/main/java/com/example/highschoolmathsolver/ui/solution/fragment/SolutionFragment.kt
+++ app/src/main/java/com/example/highschoolmathsolver/ui/solution/fragment/SolutionFragment.kt
@@ -4,12 +4,15 @@ package com.example.highschoolmathsolver.ui.solution.fragment
 import androidx.lifecycle.Observer
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.PagerAdapter
 import com.example.highschoolmathsolver.R
 import com.example.highschoolmathsolver.ui.BaseFragment
+import com.example.highschoolmathsolver.ui.dialog.GraphDialog
+import com.example.highschoolmathsolver.ui.dialog.SolutionDialogFragment
+import com.example.highschoolmathsolver.ui.solution.ShowDetailDialogListener
 import com.example.highschoolmathsolver.ui.solution.adapter.MyPagerAdapter
 import com.example.highschoolmathsolver.ui.solution.adapter.SolutionAdapter
 import com.example.highschoolmathsolver.viewmodel.SharedModel
@@ -27,13 +30,29 @@ import kotlinx.android.synthetic.main.fragment_solution.*
  * A simple [BaseFragment] subclass.
  *
  */
-class SolutionFragment : BaseFragment() {
+class SolutionFragment : BaseFragment(), ShowDetailDialogListener {
+
+    override fun onShowDetailGraph(expression: String) {
+        activity?.supportFragmentManager?.let {
+            detailGraphDialog = detailGraphDialog ?: GraphDialog(expression)
+            detailGraphDialog?.show(it, "Graph_Dialog")
+        }
+    }
+
+    override fun onShowDetailStep(expression: String) {
+        activity?.supportFragmentManager?.let {
+            detailDialog = detailDialog ?: SolutionDialogFragment(expression)
+            detailDialog?.show(it, "Dialog")
+        }
+    }
 
     private val viewModel: SharedModel by lazy {
         ViewModelProviders.of(this, viewModelFactory).get(SharedModel::class.java)
     }
 
     private val mSolutionAdapter = SolutionAdapter()
+    private var detailDialog: DialogFragment? = null
+    private var detailGraphDialog : DialogFragment? = null
 
     override val requestLayoutID: Int get() = R.layout.fragment_solution
     override fun setupFragmentComponent() = getUserComponent().inject(this)
@@ -47,12 +66,13 @@ class SolutionFragment : BaseFragment() {
         )
         recycler_view.layoutManager = layoutManager
         recycler_view.adapter = mSolutionAdapter
+        mSolutionAdapter.setShowDetailDialogListener(this)
         bindEvent()
     }
 
     private fun bindEvent() {
         viewModel.getSolutionData().observe(this, Observer {
-            empty_background.visibility = if(it.isEmpty()) View.VISIBLE else View.GONE
+            empty_background.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
             showResult(it)
         })
 
@@ -63,5 +83,11 @@ class SolutionFragment : BaseFragment() {
 
     private fun showResult(steps: List<String>) {
         mSolutionAdapter.setData(steps)
+    }
+
+    override fun onDestroyView() {
+        detailDialog?.dismiss()
+        detailGraphDialog?.dismiss()
+        super.onDestroyView()
     }
 }
