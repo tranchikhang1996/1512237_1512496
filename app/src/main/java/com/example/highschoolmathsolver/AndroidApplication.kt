21ff03d19d70593c5aa581aff10b8a18bf3875d0
khangtc2 <khangtc2@vng.com.vn>
2019-05-31 18:16:26 +0700

    [detector]: update loading for start Screen

--- app/src/main/java/com/example/highschoolmathsolver/AndroidApplication.kt
+++ app/src/main/java/com/example/highschoolmathsolver/AndroidApplication.kt
@@ -3,20 +3,31 @@ package com.example.highschoolmathsolver
 import android.app.Application
 import android.content.Context
 import androidx.room.Room
+import com.example.highschoolmathsolver.defination.SymbolType
+import com.example.highschoolmathsolver.detector.data.Gmm
+import com.example.highschoolmathsolver.detector.data.rule.BinaryRule
+import com.example.highschoolmathsolver.detector.data.rule.TerminalRule
 import com.example.highschoolmathsolver.di.component.DaggerUserComponent
 import com.example.highschoolmathsolver.di.component.UserComponent
 import com.example.highschoolmathsolver.model.database.ExpressionDatabase
+import com.example.highschoolmathsolver.util.ImageUtils
 import org.opencv.ml.ANN_MLP
 import timber.log.Timber
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 
-
 class AndroidApplication : Application() {
 
     companion object {
         const val DATABASE_NAME = "expression_history_database"
+        const val labelFileName = "label.txt"
+        const val weightFileName = "weights.txt"
+        const val gmmFileName = "sparel.txt"
+        const val terminalFile = "term.txt"
+        const val binaryFile = "binary.txt"
+        const val startSymbolFile = "startsymbol.txt"
+        const val symbolTypeFile = "symbolType.txt"
         lateinit var instance: AndroidApplication
     }
 
@@ -24,6 +35,12 @@ class AndroidApplication : Application() {
     lateinit var database: ExpressionDatabase
     lateinit var labelTable: HashMap<Int, String>
     lateinit var mlp: ANN_MLP
+    lateinit var gmm: Gmm
+    lateinit var terminalRules: MutableList<TerminalRule>
+    lateinit var binaryRules: MutableList<BinaryRule>
+    lateinit var starSymbols: MutableList<String>
+    lateinit var symbolTypes: MutableMap<String, SymbolType>
+
 
     override fun onCreate() {
         super.onCreate()
@@ -35,47 +52,31 @@ class AndroidApplication : Application() {
         }
     }
 
-    fun loadMlp() { // load du lieu , model thuoc openCv
-        if(::mlp.isInitialized) {
+    fun loadConfig() {
+        loadMlp()
+        loadLabelTable()
+        loadGmm()
+        loadBinaryRule()
+        loadTerminalRule()
+        loadStartSymbol()
+        loadSymbolType()
+    }
+
+    private fun loadMlp() { // load du lieu , model thuoc openCv
+        if (::mlp.isInitialized) {
             return
         }
-        val weightFile = File(filesDir, "weights.txt")
-        if (!weightFile.exists()) {
-            val fis = assets.open("weights.txt")
-            openFileOutput("weights.txt", Context.MODE_PRIVATE).use {
-                val buffer = ByteArray(1024)
-                var n = fis.read(buffer)
-                while (n != -1) {
-                    it.write(buffer, 0, n)
-                    n = fis.read(buffer)
-                }
-                it.close()
-            }
-            fis.close()
-        }
+        val weightFile = fromAssetToInternal(weightFileName)
         mlp = ANN_MLP.load(weightFile.path)
     }
 
-    fun loadLabelTable() { // load du lieu, model thuoc openCV
-        if(::labelTable.isInitialized) {
+    private fun loadLabelTable() { // load du lieu, model thuoc openCV
+        if (::labelTable.isInitialized) {
             return
         }
 
         labelTable = HashMap()
-        val labelFile = File(filesDir, "label.txt")
-        if (!labelFile.exists()) {
-            val fis = assets.open("label.txt")
-            openFileOutput("label.txt", Context.MODE_PRIVATE).use {
-                val buffer = ByteArray(1024)
-                var n = fis.read(buffer)
-                while (n != -1) {
-                    it.write(buffer, 0, n)
-                    n = fis.read(buffer)
-                }
-                it.close()
-            }
-            fis.close()
-        }
+        val labelFile = fromAssetToInternal(labelFileName)
 
         val b = BufferedReader(FileReader(labelFile))
         var line = b.readLine()
@@ -87,5 +88,106 @@ class AndroidApplication : Application() {
             line = b.readLine()
         }
         b.close()
+    }
+
+    private fun loadGmm() {
+        if (::gmm.isInitialized) {
+            return
+        }
+        gmm = Gmm()
+        val spaFile = fromAssetToInternal(gmmFileName)
+        gmm.loadModel(spaFile)
+    }
+
+    private fun loadTerminalRule() {
+        if (::terminalRules.isInitialized) {
+            return
+        }
+        terminalRules = arrayListOf()
+        val file = fromAssetToInternal(terminalFile)
+        val b = BufferedReader(FileReader(file))
+        var line = b.readLine()
+        while (line != null) {
+            val values = line.split(' ')
+            val rule = TerminalRule(values[1], values[2], values[0].toDouble(), values[3])
+            terminalRules.add(rule)
+            Timber.d("[loadTerminalRule] ${rule.prob} ${rule.A} ${rule.s} ${rule.template}")
+            line = b.readLine()
+        }
+        b.close()
+    }
+
+    private fun loadBinaryRule() {
+        if (::binaryRules.isInitialized) {
+            return
+        }
+        binaryRules = arrayListOf()
+        val file = fromAssetToInternal(binaryFile)
+        val b = BufferedReader(FileReader(file))
+        var line = b.readLine()
+        while (line != null) {
+            val values = line.split(' ')
+            val rule =
+                BinaryRule(
+                    values[2], values[3], values[4],
+                    values[0].toDouble(), ImageUtils.toSpatialRelationship(values[1]),
+                    values[5].replace('@',' '), ImageUtils.toMergeTag(values[6])
+                )
+            binaryRules.add(rule)
+            Timber.d("[loadRule] ${rule.prob} ${rule.sparel} ${rule.A} ${rule.B} ${rule.C} ${rule.template}")
+            line = b.readLine()
+        }
+        b.close()
+    }
+
+    private fun loadStartSymbol() {
+        if (::starSymbols.isInitialized) {
+            return
+        }
+        starSymbols = arrayListOf()
+        val file = fromAssetToInternal(startSymbolFile)
+        val b = BufferedReader(FileReader(file))
+        var line = b.readLine()
+        while (line != null) {
+            val values = line.split(' ')
+            starSymbols.add(values[0])
+            line = b.readLine()
+        }
+        b.close()
+    }
+
+    private fun loadSymbolType() {
+        if (::symbolTypes.isInitialized) {
+            return
+        }
+        symbolTypes = mutableMapOf()
+        val file = fromAssetToInternal(symbolTypeFile)
+        val b = BufferedReader(FileReader(file))
+        var line = b.readLine()
+        while (line != null) {
+            val values = line.split(' ')
+            symbolTypes[values[0]] = ImageUtils.toSymbolType(values[1])
+            line = b.readLine()
+        }
+        b.close()
+    }
+
+    private fun fromAssetToInternal(fileName: String): File {
+        val file = File(filesDir, fileName)
+        if (file.exists()) {
+            return file
+        }
+        val fis = assets.open(fileName)
+        openFileOutput(fileName, Context.MODE_PRIVATE).use {
+            val buffer = ByteArray(1024)
+            var n = fis.read(buffer)
+            while (n != -1) {
+                it.write(buffer, 0, n)
+                n = fis.read(buffer)
+            }
+            it.close()
+        }
+        fis.close()
+        return file
     }
 }
--- app/src/main/java/com/example/highschoolmathsolver/MainActivity.kt
+++ app/src/main/java/com/example/highschoolmathsolver/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.highschoolmathsolver
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.highschoolmathsolver.ui.home.activity.HomeActivity
+import com.example.highschoolmathsolver.util.DialogHelper
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
@@ -11,6 +17,8 @@ import timber.log.Timber
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private val mSubscription = CompositeDisposable()
 
     init {
         if (!OpenCVLoader.initDebug())
@@ -30,32 +38,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    val mLoaderCallback = object : BaseLoaderCallback(this) {
+
+    private fun initModel(context : Context) {
+        val disposable = Single.create<Boolean> {
+            try {
+                AndroidApplication.instance.loadConfig()
+                it.onSuccess(true)
+            } catch (e: IOException) {
+                it.onError(e)
+            }
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                val intent = Intent(context, HomeActivity::class.java)
+                context.startActivity(intent)
+            }, { t ->
+                Timber.d(t)
+                showErrorAndQuick()
+            })
+        mSubscription.add(disposable)
+    }
+
+    private fun showErrorAndQuick() {
+        DialogHelper.showError(activity = this, message = getString(R.string.main_error_message))
+    }
+
+    private val mLoaderCallback = object : BaseLoaderCallback(this) {
         override fun onManagerConnected(status: Int) {
             when (status) {
                 LoaderCallbackInterface.SUCCESS -> {
-                    try {
-                        AndroidApplication.instance.loadLabelTable()
-                        AndroidApplication.instance.loadMlp()
-                        val intent = Intent(mAppContext, HomeActivity::class.java)
-                        startActivity(intent)
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-
+                    initModel(mAppContext)
                 }
                 else -> {
                     super.onManagerConnected(status)
+                    showErrorAndQuick()
                 }
             }
-        }
-    }
-
-    external fun stringFromJNI(): String
-
-    companion object {
-        init {
-            System.loadLibrary("native-lib")
         }
     }
 }
