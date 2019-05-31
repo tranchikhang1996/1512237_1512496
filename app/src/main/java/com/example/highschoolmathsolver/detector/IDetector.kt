50a52fe830a5b3ad8f9e9e7c5375ad23b5100a09
khangtc2 <khangtc2@vng.com.vn>
2019-05-31 18:18:06 +0700

    [detector]: remove redundant codes

--- app/src/main/java/com/example/highschoolmathsolver/detector/IDetector.kt
+++
@@ -1,9 +0,0 @@
-package com.example.highschoolmathsolver.detector
-
-import android.media.Image
-import com.example.highschoolmathsolver.model.entity.Expression
-import io.reactivex.Observable
-
-interface IDetector {
-    fun detect(image : Image, previewWidth : Int, previewHeight : Int, rectWidth : Int, rectHeight : Int) : Observable<String>
-}
--- app/src/main/java/com/example/highschoolmathsolver/model/entity/Hypothesis.kt
+++
@@ -1,7 +0,0 @@
-package com.example.highschoolmathsolver.model.entity
-
-import org.opencv.core.Rect
-
-data class Hypothesis(var values: List<Pair<String, Double>>, var boundingBox: Rect) {
-    fun getBestPrediction(): Pair<String, Double> = values.maxBy { it.second } ?: Pair("", 0.0)
-}
