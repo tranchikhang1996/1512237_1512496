abf4867822da9478ac84e6c538afc059e685c5eb
1512496 <1512496@student.hcmus.edu.vn>
2019-06-13 13:54:28 +0700

    bo sung output o cac dang toan khong ho tro

--- app/src/main/java/com/example/highschoolmathsolver/mathengine/stepbystepengine/UnSupportedMathType.kt
+++ app/src/main/java/com/example/highschoolmathsolver/mathengine/stepbystepengine/UnSupportedMathType.kt
@@ -1,8 +1,13 @@
 package com.example.highschoolmathsolver.mathengine.stepbystepengine
 
+import com.example.highschoolmathsolver.util.MathUtils
+
 class UnSupportedMathType(_latexExpression : String) : MathType(_latexExpression){
     override  public fun solution(): List<String>{
-
-        return arrayListOf()
+        var rs= arrayListOf<String>()
+        var b0="<h3>- Theo đề ta có biểu thức: "+ MathUtils.trimToKaTeX(latexExpression)+"<br>"
+        b0+="Hiện ứng dụng chưa hỗ trợ dạng toán này, vui lòng thử các dạng toán khác."
+        rs.add(b0+"</h3>")
+        return rs
     }
 }
