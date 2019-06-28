108fd9a49226905aba74ead4adef73cb7664fc13
1512496 <1512496@student.hcmus.edu.vn>
2019-06-28 23:49:14 +0700

    thêm giải bài toán phương trình bằng mxParser hàm tính giá trị f(x) theo x và f(x)

--- app/src/main/java/com/example/highschoolmathsolver/mathengine/expression/FormalExpression.kt
+++ app/src/main/java/com/example/highschoolmathsolver/mathengine/expression/FormalExpression.kt
@@ -1,6 +1,6 @@
 package com.example.highschoolmathsolver.mathengine.expression
 
-interface FormalExpression {
+interface   FormalExpression {
     public abstract fun evalute(x: Double): Double
     public abstract fun derive(): FormalExpression
     public abstract fun expToString(): String
--- app/src/main/java/com/example/highschoolmathsolver/mathengine/stepbystepengine/Equation.kt
+++ app/src/main/java/com/example/highschoolmathsolver/mathengine/stepbystepengine/Equation.kt
@@ -1,16 +1,31 @@
 package com.example.highschoolmathsolver.mathengine.stepbystepengine
 
-import com.example.highschoolmathsolver.util.MathUtils
+import com.example.highschoolmathsolver.util.MathUtils.Companion.solveEquationMxParser
 import com.example.highschoolmathsolver.util.MathUtils.Companion.trimToKaTeX
+import com.example.highschoolmathsolver.util.MathUtils.Companion.trimToMxParserFx
 import org.mariuszgromada.math.mxparser.Expression
+import org.mariuszgromada.math.mxparser.Function
 
 class Equation(_latexExpression : String) : MathType(_latexExpression){
     override  public fun solution(): List<String>{
         var rs= arrayListOf<String>()
-        var b0="<h3>- Theo đề ta có phương trình: "+ trimToKaTeX(latexExpression)
+        var katexExpression= trimToKaTeX(latexExpression)
+        var b0="<h3>- Theo đề ta có phương trình: "+ katexExpression +"<br>"//+ trimToMxParserFx(katexExpression)+"<br>"
+        var b1="<h3>- Giải phương trình ta có :"
+        var mxParserFx= trimToMxParserFx(katexExpression)
+        var listRs= solveEquationMxParser(mxParserFx)
+        if(listRs.size==0){
+            b1+="<br>Phương trình vô nghiệm"
+        }else{
+            b1+="<br>Phương trình có các nghiệm:"
+            for(item in listRs){
+                b1+="$$(x="+item+")$$"
+            }
+        }
         rs.add(b0+"</h3>")
-        var b1="<h3>- Giải phương trình ta có các nghiệm sau : </h3>"
-        rs.add(b1)
+        rs.add(b1+"</h3>")
+        var b2="STATIC_GRAPH_FOR_EQUATION"+katexExpression
+        rs.add(b2)
         return rs
     }
 }
