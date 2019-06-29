8f9660162fb56fdbf833e2362dbc1577625381fa
1512496 <1512496@student.hcmus.edu.vn>
2019-06-29 10:20:52 +0700

    sửa phần chuỗi truyền theo tiền tố

--- app/src/main/java/com/example/highschoolmathsolver/mathengine/stepbystepengine/Equation.kt
+++ app/src/main/java/com/example/highschoolmathsolver/mathengine/stepbystepengine/Equation.kt
@@ -6,25 +6,26 @@ import com.example.highschoolmathsolver.util.MathUtils.Companion.trimToMxParserF
 import org.mariuszgromada.math.mxparser.Expression
 import org.mariuszgromada.math.mxparser.Function
 
-class Equation(_latexExpression : String) : MathType(_latexExpression){
-    override  fun solution(): List<String>{
-        val rs= arrayListOf<String>()
-        val katexExpression= trimToKaTeX(latexExpression)
-        val b0= "<h3>- Theo đề ta có phương trình: $katexExpression<br>"//+ trimToMxParserFx(katexExpression)+"<br>"
-        var b1="<h3>- Giải phương trình ta có :"
-        val mxParserFx= trimToMxParserFx(katexExpression)
-        val listRs= solveEquationMxParser(mxParserFx)
-        if(listRs.size==0){
-            b1+="<br>Phương trình vô nghiệm"
-        }else{
-            b1+="<br>Phương trình có các nghiệm:"
-            for(item in listRs){
-                b1+= "$$(x=$item)$$"
+class Equation(_latexExpression: String) : MathType(_latexExpression) {
+    override fun solution(): List<String> {
+        var rs = arrayListOf<String>()
+        var katexExpression = trimToKaTeX(latexExpression)
+        var b0 =
+            "<h3>- Theo đề ta có phương trình: $katexExpression<br>"//+ trimToMxParserFx(katexExpression)+"<br>"
+        var b1 = "<h3>- Giải phương trình ta có :"
+        var mxParserFx = trimToMxParserFx(katexExpression)
+        var listRs = solveEquationMxParser(mxParserFx)
+        if (listRs.size == 0) {
+            b1 += "<br>Phương trình vô nghiệm"
+        } else {
+            b1 += "<br>Phương trình có các nghiệm:"
+            for (item in listRs) {
+                b1 += "$$(x=$item)$$"
             }
         }
         rs.add("$b0</h3>")
         rs.add("$b1</h3>")
-        val b2= "STATIC_GRAPH_FOR_EQUATION$katexExpression"
+        var b2 = "STATIC_GRAPH_FOR_EQUATION$latexExpression"
         rs.add(b2)
         return rs
     }
